bfee306109dfc0df2937f48fb927a53ac4041e78
Pankajr <pankaj.ray@ecanarys.net>
2018-11-22 12:38:30 +0530

    reverse function added.

--- To-do-list-application/ViewController.swift
+++ To-do-list-application/ViewController.swift
@@ -15,10 +15,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print(message)
-
+        print(reverse(text: "stressed"))
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    func reverse(text: String) -> String {
+        return String(text.reversed())
+    }
 
 }
 
